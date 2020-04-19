ab170293fdc9f423fc3400faa5e3f500097fdc93
javilorca <33719770+javilorca@users.noreply.github.com>
2020-04-19 14:55:54 +0200

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+//Ip del pc servidor BBDD
+define("DB_HOST", "localhost");
+
+//Nombre de la BBDD
+define("DB_NAME", "dbsistema");
+
+//Usuario de la BBDD
+define("DB_USERNAME", "root");
+
+//Contraseña del usuario de la BBDD
+define("DB_PASSWORD", "");
+
+//Codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+//Constante como nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+ ?>
